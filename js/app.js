209ab524c9b1fb84dba4654eb136f1d5d69b6cdc
clint601 <clintjackson601@gmail.com>
2024-12-06 15:12:00 -0600

    done

--- js/app.js
+++ js/app.js
@@ -1,56 +1,18 @@
-// const onButton  = document.getElementById('btnOn')
-// const offButton = document.getElementById('btnOff')
 
-// let offButton
-
-
-
-// document.querySelector('btn-off').addEventListener('click', 
-//     function() {
-//         document.querySelector('.btn-off').style.background = 'red'
-//         document.querySelector('.btn-on').style.background = 'green'
-//         document.querySelector('.lightbulb img').src = 'image/lighton.jpg'
-
-// })
-
-// document.querySelector('btn-on').addEventListener('click', 
-//     function() {
-//         document.querySelector('.btn-on').style.background = 'red'
-//         document.querySelector('.btn-on').style.background = 'green'
-//         document.querySelector('.lightbulb img').src = 'image/lighton.jpg'
-
-// })
+document.querySelector('.btn-off').addEventListener('click', 
+    function() {
+            document.querySelector('.btn-off').style.background = 'red'
+            document.querySelector('.btn-on').style.background = 'green'
+            document.querySelector('.lightbulb img').src = 'image/lightoff.jpg'
+    
+    })
+    
+    document.querySelector('.btn-on').addEventListener('click', 
+        function() {
+                document.querySelector('.btn-on').style.background = '#00ff00'
+                document.querySelector('.btn-on').style.background = 'green'
+                document.querySelector('.lightbulb img').src = 'image/lighton.jpg'
+        
+        })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let lightIsOn = true
-
-// if (lightIsOn) {
-//     lightIsOn = !lightIsOn
-// } else {
-//     lightIsOn = true 
-// }
-
-// console.log(`The light is on?:`, lightIsOn)
-
-
-// lightIsOn ? lightIsOn = !lightIsOn : lightIsOn = true
-
-// console.log('The is on:', lightIsOn);
